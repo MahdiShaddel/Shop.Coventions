f45303652afc42a1450bb870bed239a18152d043
Mahdi Shaddel <M.Shaddel@Software.co>
2021-01-24 14:08:03 +0330

    Add project files.

---
+++ Shop/DTOs/Users/LoginDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Host.DTOs.Users
+{
+    public class LoginDTO
+    {   
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
